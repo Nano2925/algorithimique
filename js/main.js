1a3059c7563f806043c5bbbb643031209018fc32
manoj <gnanavel.manoj29@gmail.com>
2024-02-13 16:23:00 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/* ------------------------------------ Les Commentaires ------------------------------------*/
+
+
+// un comaintaire
+
+/*  
+    un commentaire multiligne 
+    en javascript 
+*/
+
+/* ------------------------------------------- Affichage ------------------------------------*/
+
+//Afficher des informations dans la consonle de debbugage du navigateur
+console.log('hello, World') 
+
+/* --------------------------------------- Les Variables ------------------------------------*/
+// Declarer d'une variable
+let uneVariable
+
+// Affectation d'une variable
+uneVariable = 'Je suis une Variable'
+
+console.log(uneVariable)
+
+
+// Les 2 se font généralement en meme temps
+// Les variables sont sensibles à la casse 
+let helloworld = 'Hello, World'
+
+console.log(helloworld)
+
+/* ----------------------------------------- La Concatenation ----------------------------------*/
+let firstName = 'manoj'
+let lastName = 'gnanavel'
+
+// Je m'appelle (firstName ,lastName)
+console.log("je m'appelle"+ firstName + lastName);
+
+// Déclarer une variable city, afficher le méssage j'adore vivre à <city>
+ let city = 'paris'
+ city = `paris`
+ city = "paris"
+
+ console.log(`j'adore vivre à ${city}`)
+ console.log(`j'adore vivre à ${city} et je m'appelle ${firstName} ${lastName}`)
+ 
